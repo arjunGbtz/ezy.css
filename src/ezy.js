7c0599fedbfec5809b144002f1cc1bd03dfda8a6
ArjunGbtz <arjun.gaubatz@gmail.com>
2025-09-09 16:13:00 +0200

    way more flexbox features

--- src/ezy.js
+++ src/ezy.js
@@ -1,15 +1,11 @@
-// lib/ezy/ezy.js
 document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
+        entry.target.style.transitionDelay = entry.target.dataset.delay || "0s";
         entry.target.classList.add("visible");
-        // Optional: Danach nicht mehr beobachten, damit es nur einmal animiert
-        observer.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.1 });
-
-  // Alle animierbaren Klassen beobachten
+  }, { threshold: 0.1, rootMargin: "0px", });
   document.querySelectorAll(".slide-up, .slide-left, .slide-right, .fade-in").forEach(el => observer.observe(el));
 });
